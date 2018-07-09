c1104488ae4cde54f2f3892b5c2e8e91e9b7688e
liushilin <liushilin@jd.com>
2018-07-09 15:15:21 +0800

    setting

--- jtf_extension/trade_lifeservice_toc/src/main/java/com/jd/jtf/lifeservice/action/LifeserviceBuyNowAction.java
+++ jtf_extension/trade_lifeservice_toc/src/main/java/com/jd/jtf/lifeservice/action/LifeserviceBuyNowAction.java
@@ -1,4 +1,5 @@
-package com.jd.jtf.route;
+package com.jd.jtf.lifeservice.action;//package com.jd.jtf.lifeservice.action;
+
 
 import com.alibaba.fastjson.JSONObject;
 import com.jd.jtf.domain.order.Order;
@@ -9,33 +10,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/jtf")
-public class BuyNowAction {
-
+public class LifeserviceBuyNowAction {
     @ResponseBody
     @RequestMapping("/buy_general")
     public JSONObject handleRequest() {
 
 
-        Order order = new Order("general", 1);
-
-        //toc接口通过order的getAdapter的方式返回，在上面toc模快里TocAdapterFactory里，
-        // 解释了会根据order的type类型，返回对应的实现，这里会返回GenrealToc的实现。
+        Order order = new Order("lifeservice", 1);
         Toc toc = (Toc) order.getAdapter(Toc.class);
-
-        if (toc != null) {
-            toc.setOrderConfirmTimeout(order);
-            toc.setOrderConsignmentTimeout(order);
-        }
-
-//        Ump ump = (Ump)order.getAdapter(Ump.class);
-//        if (ump!=null) {
-//            ump.setOrderPromation(order);
-//        }
+        toc.setOrderConfirmTimeout(order);
+        toc.setOrderConsignmentTimeout(order);
 
         int consignment_timeout = toc.getOrderConsignmentTimeout(order);
         int confirm_timeout = toc.getOrderConfirmTimeout(order);
-
-//        float promotion = ump.getOrderPromation(order);
 
         JSONObject jo = new JSONObject();
 
@@ -44,5 +31,6 @@ public class BuyNowAction {
 //        jo.put("promotion",promotion);
 
         return jo;
+
     }
 }
--- jtf_extension/trade_lifeservice_toc/src/main/java/com/jd/jtf/lifeservice/action/RestLifeserviceBuyNowAction.java
+++ jtf_extension/trade_lifeservice_toc/src/main/java/com/jd/jtf/lifeservice/action/RestLifeserviceBuyNowAction.java
@@ -1,61 +1,61 @@
-package com.jd.jtf.lifeservice.action;
-
-import com.google.inject.Inject;
-import org.apache.camel.CamelContext;
-
-public class RestLifeserviceBuyNowAction extends BaseRestHandler {
-
-    private final Version version;
-    private final CamelContext camelContext;
-
-    @Inject
-    public RestLifeserviceBuyNowAction(Settings settings, Version version, RestController controller, Client client, CamelContext camelContext) {
-        super(settings, controller, client);
-        this.version = version;
-        this.camelContext = camelContext;
-        controller.registerHandler(GET, "/buy_lifeservice", this);
-        controller.registerHandler(HEAD, "/buy_lifeservice", this);
-    }
-
-    @Override
-    public void handleRequest(final RestRequest request, RestChannel channel, final Client client) throws Exception {
-
-        RestStatus status = RestStatus.OK;
-        if (request.method() == RestRequest.Method.HEAD) {
-            channel.sendResponse(new BytesRestResponse(status));
-            return;
-        }
-
-        XContentBuilder builder = channel.newBuilder();
-
-        // Default to pretty printing, but allow ?pretty=false to disable
-        if (!request.hasParam("pretty")) {
-            builder.prettyPrint().lfAtEnd();
-        }
-
-        Order order = new Order("lifeservice",1);
-        Toc toc = (Toc)order.getAdapter(Toc.class);
-        toc.setOrderConfirmTimeout(order);
-        toc.setOrderConsignmentTimeout(order);
-
-        Ump ump = (Ump)order.getAdapter(Ump.class);
-        ump.setOrderPromation(order);
-
-        int consignment_timeout = toc.getOrderConsignmentTimeout(order);
-        int confirm_timeout = toc.getOrderConfirmTimeout(order);
-
-        float promotion = ump.getOrderPromation(order);
-
-        builder.startObject();
-        if (settings.get("name") != null) {
-            builder.field("name", settings.get("name"));
-        }
-        builder.startObject("version")
-                .field("confignment_timeout", consignment_timeout)
-                .field("confirm_timeout",confirm_timeout)
-                .field("promotion",promotion)
-                .endObject();
-        builder.endObject();
-        channel.sendResponse(new BytesRestResponse(status, builder));
-    }
-}
+//package com.jd.jtf.lifeservice.action;
+//
+//import com.google.inject.Inject;
+//import org.apache.camel.CamelContext;
+//
+//public class RestLifeserviceBuyNowAction extends BaseRestHandler {
+//
+//    private final Version version;
+//    private final CamelContext camelContext;
+//
+//    @Inject
+//    public RestLifeserviceBuyNowAction(Settings settings, Version version, RestController controller, Client client, CamelContext camelContext) {
+//        super(settings, controller, client);
+//        this.version = version;
+//        this.camelContext = camelContext;
+//        controller.registerHandler(GET, "/buy_lifeservice", this);
+//        controller.registerHandler(HEAD, "/buy_lifeservice", this);
+//    }
+//
+//    @Override
+//    public void handleRequest(final RestRequest request, RestChannel channel, final Client client) throws Exception {
+//
+//        RestStatus status = RestStatus.OK;
+//        if (request.method() == RestRequest.Method.HEAD) {
+//            channel.sendResponse(new BytesRestResponse(status));
+//            return;
+//        }
+//
+//        XContentBuilder builder = channel.newBuilder();
+//
+//        // Default to pretty printing, but allow ?pretty=false to disable
+//        if (!request.hasParam("pretty")) {
+//            builder.prettyPrint().lfAtEnd();
+//        }
+//
+//        Order order = new Order("lifeservice",1);
+//        Toc toc = (Toc)order.getAdapter(Toc.class);
+//        toc.setOrderConfirmTimeout(order);
+//        toc.setOrderConsignmentTimeout(order);
+//
+//        Ump ump = (Ump)order.getAdapter(Ump.class);
+//        ump.setOrderPromation(order);
+//
+//        int consignment_timeout = toc.getOrderConsignmentTimeout(order);
+//        int confirm_timeout = toc.getOrderConfirmTimeout(order);
+//
+//        float promotion = ump.getOrderPromation(order);
+//
+//        builder.startObject();
+//        if (settings.get("name") != null) {
+//            builder.field("name", settings.get("name"));
+//        }
+//        builder.startObject("version")
+//                .field("confignment_timeout", consignment_timeout)
+//                .field("confirm_timeout",confirm_timeout)
+//                .field("promotion",promotion)
+//                .endObject();
+//        builder.endObject();
+//        channel.sendResponse(new BytesRestResponse(status, builder));
+//    }
+//}
