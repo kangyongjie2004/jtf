2b14b5a18ec69f52bcfa5edb1f83b2d52710608f
liushilin <liushilin@jd.com>
2018-07-09 16:10:35 +0800

    add

--- jtf_trade/trade_toc/src/main/java/com/jd/jtf/toc/TocName.java
+++ jtf_trade/trade_toc/src/main/java/com/jd/jtf/toc/TocName.java
@@ -1,4 +1,9 @@
 package com.jd.jtf.toc;
 
 public class TocName {
+    private String topName;
+
+    public TocName(String topName) {
+        this.topName = topName;
+    }
 }
--- jtf_trade/trade_toc/src/main/java/com/jd/jtf/toc/TocsService.java
+++ jtf_trade/trade_toc/src/main/java/com/jd/jtf/toc/TocsService.java
@@ -1,9 +1,34 @@
 package com.jd.jtf.toc;
 
+import com.jd.jtf.toc.general.GeneralToc;
+import com.jd.jtf.toc.virtual.VirtualToc;
+
 public class TocsService {
 
+
+    static Toc generalToc = null;
+    static Toc virtualToc = null;
+    static String type_general = "general";
+    static String type_virtual = "virtual";
+
+    static {
+
+        generalToc = new GeneralToc(new TocName(type_general), new TocSettings());
+        virtualToc = new VirtualToc(new TocName(type_virtual), new TocSettings());
+    }
+
     public Toc toc(String type) {
-        return null;
+
+        Toc toc;
+        if (type.equals(type_general)) {
+            toc = generalToc;
+        } else if (type.equals(type_virtual)) {
+            toc = virtualToc;
+        } else {
+            toc = generalToc;
+        }
+
+        return toc;
     }
 
 
